cd7e52706894fde927bc23ca9c10e03c0e9b07e1
藍天駿 <jay183145@gmail.com>
2022-05-18 11:51:39 +0800

    setup

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form>
+    <input type="text" required placeholder="display name" v-model="displayName">
+    <input type="email" required placeholder="email" v-model="email">
+    <input type="password" required placeholder="password" v-model="password">
+    <button>Sign up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+  export default {
+    setup() {
+      
+      const displayName = ref('')
+      const email = ref('')
+      const password = ref('')
+
+      return { displayName, email, password }
+    }
+    
+  }
+</script>
+
+<style>
+
+</style>
