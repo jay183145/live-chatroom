0a9206726363a41f9384df540b1b8c6bd473f4bb
藍天駿 <jay183145@gmail.com>
2022-05-20 13:19:50 +0800

    chatroom forn

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="container">
     <Navbar />
+    <NewChatForm />
   </div>
 </template>
 
 <script>
-
+import NewChatForm from '@/components/NewChatForm.vue'
 import Navbar from '../components/Navbar.vue'
 import { useRouter } from 'vue-router'
 import getUser from '@/composables/getUser'
 import { watch } from '@vue/runtime-core'
 
 export default {
-  components: { Navbar },
+  components: { Navbar, NewChatForm },
   setup() {
     const { user } = getUser()
     const router = useRouter()
